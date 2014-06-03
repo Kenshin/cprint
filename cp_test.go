dc314c8b04a5f8e61ff789fd5ea762e4bc4cad58
Kenshin <fxblog@gmail.com>
2014-06-03 12:40:11 +0800

    modify cp.go and cp_test.go

--- cp_test.go
+++ cp_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestP(t *testing.T) {
+func TestCP(t *testing.T) {
 
 	// test ont
 	P(WARING, "Remote latest version %v %v latest version %v.\n", "0.10.28", "=", "0.1.0.26")
@@ -14,6 +14,7 @@ func TestP(t *testing.T) {
 	cp := CP{Red, false, None, false, "="}
 	P(NOTICE, "Remote latest version %v %v latest version %v, don't need to upgrade.\n", "0.10.28", cp, "0.1.0.26")
 
+	// test three
 	err := errors.New("Variable is not defined.")
 	Error(ERROR, "'gnvm updte latest' an error has occurred. \nError: ", err)
 }
